4b0de510f16e8ec2434d8ad8e37894105b9900d8
Rodrigo Cioletti <rlcioletti@gmail.com>
2024-01-26 02:50:59 -0500

    pipelineInfo handled but missing renderPass and pipelineLayout

--- VulkanRenderer/vre_pipeline.hpp
+++ VulkanRenderer/vre_pipeline.hpp
@@ -8,8 +8,18 @@
 namespace vre {
 	
 	struct PipelineConfigInfo {
-
-
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
 	};
 
 	class VrePipeline {
