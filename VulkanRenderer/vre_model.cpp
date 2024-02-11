7811bd2b72bb05939430bc87dee731e9f632e4ba
Rodrigo Cioletti <rlcioletti@gmail.com>
2024-02-11 01:05:05 -0500

    Used tinyobjloader to load 3d models from disk

--- VulkanRenderer/vre_model.cpp
+++ VulkanRenderer/vre_model.cpp
@@ -1,7 +1,28 @@
 #include "vre_model.hpp"
 
+#include "vre_utils.hpp"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
 #include <cassert>
 #include <cstring>
+#include <unordered_map>
+
+namespace std {
+	template <>
+	struct hash<vre::VreModel::Vertex> {
+
+		size_t operator()(vre::VreModel::Vertex const& vertex)  const {
+			size_t seed = 0;
+			vre::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+
+			return seed;
+		}
+	};
+}
 
 namespace vre {
 
@@ -33,6 +54,14 @@ namespace vre {
 
 			vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
 		}
+	}
+
+	std::unique_ptr<VreModel> VreModel::createModelFromFile(VreDevice& device, const std::string& filepath)
+	{
+		Builder builder;
+		builder.loadModel(filepath);
+
+		return std::make_unique<VreModel>(device, builder);
 	}
 
 	void VreModel::bind(VkCommandBuffer commandBuffer)
@@ -143,5 +172,70 @@ namespace vre {
 
 
 		return attributeDescriptions;
+	}
+	void VreModel::Builder::loadModel(const std::string& filepath)
+	{
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string warn, err;
+
+		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filepath.c_str())) {
+			throw std::runtime_error(warn + err);
+		}
+
+		vertices.clear();
+		indices.clear();
+
+		std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
+		for (const auto& shape : shapes) {
+			for (const auto& index : shape.mesh.indices) {
+				Vertex vertex{};
+
+				if (index.vertex_index >= 0) {
+					vertex.position = {
+						attrib.vertices[3 * index.vertex_index + 0],
+						attrib.vertices[3 * index.vertex_index + 1],
+						attrib.vertices[3 * index.vertex_index + 2],
+					};
+
+					auto colorIndex = 3 * index.vertex_index + 2;
+					
+					if (colorIndex < attrib.colors.size()) {
+						vertex.color = {
+							attrib.colors[colorIndex - 0],
+							attrib.colors[colorIndex - 1],
+							attrib.colors[colorIndex - 2],
+						};
+					}
+					else {
+						vertex.color = { 1.f, 1.f, 1.f };
+					}
+				}
+
+				if (index.normal_index >= 0) {
+					vertex.normal = {
+						attrib.normals[3 * index.normal_index + 0],
+						attrib.normals[3 * index.normal_index + 1],
+						attrib.normals[3 * index.normal_index + 2],
+					};
+				}
+
+				if (index.texcoord_index >= 0) {
+					vertex.uv = {
+						attrib.texcoords[2 * index.texcoord_index + 0],
+						attrib.texcoords[2 * index.texcoord_index + 1],
+					};
+				}
+
+				if (uniqueVertices.count(vertex) == 0) {
+					uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+					vertices.push_back(vertex);
+				}
+				
+				indices.push_back(uniqueVertices[vertex]);
+			}
+		}
 	}
 }
--- VulkanRenderer/vre_model.hpp
+++ VulkanRenderer/vre_model.hpp
@@ -7,6 +7,7 @@
 #include <glm/glm.hpp>
 
 #include <vector>
+#include <memory>
 
 namespace vre {
 	class VreModel {
@@ -14,17 +15,25 @@ namespace vre {
 
 		struct Vertex {
 
-			glm::vec3 position;
-			glm::vec3 color;
+			glm::vec3 position{};
+			glm::vec3 color{};
+			glm::vec3 normal{};
+			glm::vec2 uv{};
 
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+			bool operator==(const Vertex& other) const {
+				return position == other.position && color == other.color && normal == other.normal && uv == other.uv;
+			}
 		};
 
 		struct Builder {
 
 			std::vector<Vertex> vertices{};
 			std::vector<uint32_t> indices{};
+
+			void loadModel(const std::string& filepath);
 		};
 
 		VreModel(VreDevice &device, const VreModel::Builder &builder);
@@ -32,6 +41,8 @@ namespace vre {
 
 		VreModel(const VreModel&) = delete;
 		VreModel& operator=(const VreModel&) = delete;
+
+		static std::unique_ptr<VreModel> createModelFromFile(VreDevice& device, const std::string& filepath);
 
 		void bind(VkCommandBuffer commandBuffer);
 		void draw(VkCommandBuffer commandBuffer);
